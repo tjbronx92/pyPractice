b25b5c8394a5535392c059128d26585a3b41535b
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2024-05-27 16:10:49 -0700

    Make game more challenging
    User only has 3 chances to guess secret number

--- Random_Num_Game.py
+++ Random_Num_Game.py
@@ -9,24 +9,30 @@ def guessing_game():
     #Computer picks a number between 1 & 100
     number
     
+    chances = 3
+    
     #Ask user to take a guess as INPUT
-    print("Guess a number bertween 1 - 100, NOW!\n")
+    print("Guess a number bertween 1 - 100...\n You Only Have 3 Chances!!!\n")
     guess = input("No Decimels, Archimedes...\n")
     
-    while True:
+    while chances >= 2:
         #Check if user was correct
         if int(guess) < number:
             print(f"{guess} Is To Low...")
-            guess = input("Guess Again! \n")
+            chances -= 1
+            guess = input(f"Guess Again!\n You have {chances} chance left\n")
         elif int(guess) > number:
             print(f"{guess} Is To High...")
-            guess = input("Guess Again! \n")
+            chances -= 1
+            guess = input(f"Guess Again!\n You have {chances} chance left\n")
         elif int(guess) == number:
             print(f"{guess} Was Just Right!")
             return()
         else:
             print("Input is invalid. Check yourself!")
-            guess = input("Guess Again! \n")
+            guess = input("Guess Again! You have {chances} left\n")
+        print("You Lose Out Of Guesses")
+    return()
         
 #START GAME            
 guessing_game()
