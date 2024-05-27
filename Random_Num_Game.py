e10073e6cd7a420b566bfbad16f301878ef42aa5
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2024-05-27 15:32:51 -0700

    Add files via upload

---
+++ Random_Num_Game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+number=random.randint(1, 101)
+
+def guessing_game():
+    #Computer picks a number between 1 & 100
+    number
+    
+    #Ask user to take a guess as INPUT
+    print("Guess a number bertween 1 - 100, NOW!")
+    guess = input()
+    
+    while True:
+        #Check if user was correct
+        if int(guess) < number:
+            print(f"{guess} Is To Low...")
+            guess = input("Guess Again!")
+        elif int(guess) > number:
+            print(f"{guess} Is To High...")
+            guess = input("Guess Again!")
+        elif int(guess) == number:
+            print(f"{guess} Was Just Right!")
+            exit()
+        else:
+            print("Input is invalid. Check yourself!")
+            guess = input()
+        
+guessing_game()
+
+"""
+Created on Mon May 27 14:58:15 2024
+
+@author: tj
+"""
+
