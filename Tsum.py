e9979df7d41792174db499cd7589821f475d0d47
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2024-05-29 21:19:52 -0700

    Practice creating a version of sum()

---
+++ Tsum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Homespun version of sum()
+
+
+def Tsum(*numbers):
+    output = 0
+    for num in numbers:
+        output+=num
+    return(output)
+    
+
+"""
+Created on Wed May 29 21:08:08 2024
+
+@author: tj
+"""
+
